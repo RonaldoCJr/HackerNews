5133d599a35cb6130a6462b98c6f03008024431d
Ronaldo Carrasco <rcjks@hotmail.com>
2026-05-26 09:23:40 -0300

    removing outputcache

--- API/Controllers/v1/StoryController.cs
+++ API/Controllers/v1/StoryController.cs
@@ -2,7 +2,6 @@
 using HackerNews.Application.Interfaces;
 using HackerNews.Application.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.OutputCaching;
 
 namespace HackerNews.API.Controllers.v1;
 
@@ -24,7 +23,6 @@ public class StoryController : ControllerBase
     /// </summary>
     /// <param name="n">The number of top stories to retrieve. Must be greater than zero.</param>
     [HttpGet("bests/limit={n}")]
-    [OutputCache]
     [ProducesResponseType(typeof(IEnumerable<StoryDTO>), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status502BadGateway)]
