08fc88388939faeaecfc1ba39e2c0e1d9910be72
Ronaldo Carrasco <rcjks@hotmail.com>
2026-05-26 09:18:58 -0300

    removing commentary

--- API/Controllers/v1/StoryController.cs
+++ API/Controllers/v1/StoryController.cs
@@ -19,30 +19,6 @@ public class StoryController : ControllerBase
         _storyService = storyService;
     }
 
-    ///// <summary>
-    ///// Retrieves the top 'n' stories from Hacker News, ordered by their score in descending order.
-    ///// </summary>
-    ///// <remarks>
-    ///// This endpoint fetches all current best stories, retrieves their individual details in parallel, 
-    ///// and sorts them by score to ensure accuracy. Results are cached to optimize performance.
-    ///// </remarks>
-    ///// <param name="n">The number of top stories to retrieve. Must be greater than zero.</param>
-    ///// <returns>A list of the best stories with titles, scores, and comment counts.</returns>
-    ///// <response code="200">Returns the list of best stories successfully.</response>
-    ///// <response code="400">If the provided 'n' is less than or equal to zero.</response>
-    ///// <response code="502">If there is an error communicating with upstream Hacker News API or Circuit Breaker triggered.</response>
-    ///// <response code="504">If the request to the upstream Hacker News API times out.</response>
-    ///// <response code="500">If an unhandled error occurs internally within the server.</response>
-    //[HttpGet("bests/limit={n}")]
-    //[OutputCache]
-    //public async Task<IActionResult> GetBests(int n)
-    //{
-    //    if (n <= 0)
-    //        return BadRequest("n must be > 0");
-
-    //    return Ok(await _storyService.GetBestsAsync(n));
-    //}
-
     /// <summary>
     /// Retrieves the top 'n' stories from Hacker News, ordered by score descending.
     /// </summary>
